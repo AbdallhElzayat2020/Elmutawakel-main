e6d430fd7edb2ddfa4dd9304f28be908e595c198
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2023-05-25 12:56:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@
 // ScrollReveal().reveal('.logo', {
 //     origin: "top"
 // });
+
+const btn = document.querySelector(".scroll-top")
+const btn2 = document.querySelector(".scroll-left");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 150) {
+    btn.style.display = "block";
+    btn2.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    btn2.style.display = "none";
+  };
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+btn2.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
